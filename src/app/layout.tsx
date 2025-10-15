fd3cd70f52743eabdb8e9bbe9e7dd2e332e8565d
wyatt-connolly <wyattconnollydev@gmail.com>
2025-10-15 15:31:01 -0700

    Add padding under banner for better spacing on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Banner />
         <Navigation />
-        {children}
+        <div className="pt-16">{children}</div>
         <Footer />
       </body>
     </html>
